8ebd9afb043f8195b8d6bc1791aa54573ffaacbe
numarako <s12257010@gmail.com>
2021-10-14 14:31:20 +0900

    migration change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_052204) do
+ActiveRecord::Schema.define(version: 2021_10_05_052205) do
 
   create_table "foods", force: :cascade do |t|
     t.integer "restaurant_id", null: false
