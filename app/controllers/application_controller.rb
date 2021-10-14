d02f02eaba6c1f3c1370a2874c0b37560921dbd6
numarako <s12257010@gmail.com>
2021-10-14 11:19:35 +0900

    settings for React Router

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::API
   def fake_load
     sleep(1)
   end
+
+  #HerokuでReact Routerを使用するための設定
+  include ActionController::MimeResponds
+    def fallback_index_html
+        respond_to do |format|
+            format.html { render body: Rails.root.join('public/index.html').read }
+        end
+    end
 end
