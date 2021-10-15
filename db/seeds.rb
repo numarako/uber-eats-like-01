a5a6b8b6991bd83901f560eebc75543b240c78de
numarako <s12257010@gmail.com>
2021-10-15 09:34:41 +0900

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-3.times do |n|
+2.times do |n|
   restaurant = Restaurant.new(
     name: "testレストラン_#{n}",
     fee: 100,
