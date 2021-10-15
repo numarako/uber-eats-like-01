714c98623d0ddff577feb36f40436f3dffc46d81
numarako <s12257010@gmail.com>
2021-10-15 09:03:10 +0900

    test3 https

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://aqueous-plains-12758.herokuapp.com'
+    origins 'http://localhost:3000', 'https://aqueous-plains-12758.herokuapp.com'
 
     resource '*',
       headers: :any,
--- frontend/src/urls/index.js
+++ frontend/src/urls/index.js
@@ -1,4 +1,4 @@
-const DEFAULT_API_LOCALHOST = 'http://aqueous-plains-12758.herokuapp.com/api/v1';
+const DEFAULT_API_LOCALHOST = 'https://aqueous-plains-12758.herokuapp.com/api/v1';
 
 export const restaurantsIndex = `${DEFAULT_API_LOCALHOST}/restaurants`;
 export const foodsIndex = (restaurantId) =>
