64b25eb006042214635af1ae4e3a9ef11b3c06e6
numarako <s12257010@gmail.com>
2021-12-30 16:29:18 +0900

    create

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,27 @@
 module Api
   module V1
     class RecipesController < ApplicationController
+      def create
+        recipe = Recipe.new(
+          "title": params[:title],
+          "making_time": params[:making_time],
+          "serves": params[:serves],
+          "ingredients": params[:ingredients],
+          "cost": params[:cost],
+        )
+        if recipe.save
+          render json: {
+            message: "Recipe successfully created!",
+            recipe: recipe
+          }, status: :200
+        else
+          render json: {
+            message: "Recipe creation failed!",
+            required: "title, making_time, serves, ingredients, cost"
+          }, status: :200
+        end
+      end
+
       def index
         recipes = Recipe.all
 
