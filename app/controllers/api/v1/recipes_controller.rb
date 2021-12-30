7b6e362868f9c401139ef78cf36ed0ebc1edd32e
numarako <s12257010@gmail.com>
2021-12-30 17:49:09 +0900

    FCC

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,10 +1,21 @@
 module Api
   module V1
     class RecipesController < ApplicationController
-      rescue_from ActionController::ParameterMissing, with: :handle_parameter_missing
-
       def create
-        recipe = Recipe.new(user_search_params)
+        recipe = Recipe.new(
+          "title": params[:title],
+          "making_time": params[:making_time],
+          "serves": params[:serves],
+          "ingredients": params[:ingredients],
+          "cost": params[:cost],
+        )
+       if params[:serves].nil?
+        render json: {
+          message: "Recipe creation failed!",
+          required: "title, making_time, serves, ingredients, cost"
+        }, status: :ok
+       end
+
         if recipe.save
           render json: {
             message: "Recipe successfully created!",
@@ -24,15 +35,6 @@ module Api
         render json: {
           recipes: recipes
         }, status: :ok
-      end
-
-      def user_search_params
-        params.require(:title, :making_time, :serve, :ingredients, :cost)
-        params.permit(:title, :making_time, :serve, :ingredients, :cost)
-      end
-    
-      def handle_parameter_missing(e)
-        render json: { message: e.original_message }, status: 200
       end
     end
   end
