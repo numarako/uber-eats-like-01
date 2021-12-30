5bddb5a74207abaafcd7458b8a6a3d100a19e35e
numarako <s12257010@gmail.com>
2021-12-30 16:42:13 +0900

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://aqueous-plains-12758.herokuapp.com'
+    #origins 'http://localhost:3000', 'https://aqueous-plains-12758.herokuapp.com'
+    origins '*'
 
     resource '*',
       headers: :any,
